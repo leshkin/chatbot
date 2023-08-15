b949eb96039ace040dc1d93f869da10a8cd60794
leshkin <alaposhin@gmail.com>
2023-08-15 13:03:01 +0300

    release: first version

---
+++ README.md
@@ -0,0 +1,32 @@
+# Telegram chatbot with ChatGPT support
+
+## Before launching the chatbot
+
+Create a bot in [Telegram](https://core.telegram.org/bots#how-do-i-create-a-bot) and an account in [OpenAI](https://platform.openai.com/signup)
+
+## Usage
+
+1. Rename `.env.example` to `.env` and specify environment variables.
+2. Install dependencies
+
+```
+npm i
+```
+
+3. Start chatbot locally
+
+```
+npm run dev
+```
+
+## Deployment
+
+1. Install Docker on the server
+2. Create and push Docker images to Docker Hub
+3. Rename `docker-compose.example.yml` to `docker-compose.yml` and specify an image name.
+4. Copy `docker-compose.yml` and `.env` to a folder on the server
+5. Start container
+
+```
+docker compose up -d
+```
---
+++ bot.ts
@@ -0,0 +1,73 @@
+import { Bot, Context, session, SessionFlavor, MemorySessionStorage } from 'grammy'
+import { Configuration, OpenAIApi } from 'openai'
+import 'dotenv/config'
+
+const ALLOWED_USERS = process.env.ALLOWED_USERS as string
+const MAX_MESSAGES_IN_HISTORY = Number(process.env.MAX_MESSAGES_IN_HISTORY)
+
+interface Message {
+  role: 'system' | 'user' | 'assistant'
+  content: string
+}
+
+interface SessionData {
+  messages: Array<Message>
+}
+
+type MyContext = Context & SessionFlavor<SessionData>
+
+const bot = new Bot<MyContext>(process.env.TELEGRAM_BOT_TOKEN as string)
+
+function createInitialSessionData(): SessionData {
+  return { messages: [{ role: 'system', content: process.env.OPENAI_BOT_ROLE as string }] }
+}
+
+bot.use(
+  session({
+    initial: createInitialSessionData,
+    storage: new MemorySessionStorage(Number(process.env.MESSAGE_HISTORY_RETENTION_TIME)),
+  })
+)
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+bot.command('start', (ctx) => {
+  ctx.reply(process.env.MESSAGE_START as string)
+})
+
+bot.on('message', async (ctx) => {
+  const username = ctx.message.from.username as string
+  const text = ctx.message.text as string
+
+  console.log('user: ' + username)
+  console.log('text: ' + text)
+
+  if (ALLOWED_USERS.split(',').indexOf(username) > -1) {
+    ctx.session.messages.push({ role: 'user', content: text })
+
+    const response = await openai.createChatCompletion({
+      model: process.env.OPENAI_MODEL as string,
+      messages: ctx.session.messages,
+    })
+
+    const reply = response.data.choices[0].message?.content as string
+    ctx.session.messages.push({ role: 'assistant', content: reply })
+
+    if (ctx.session.messages.length > MAX_MESSAGES_IN_HISTORY) {
+      ctx.session.messages.shift()
+      ctx.session.messages.shift()
+    }
+
+    console.log('reply: ' + reply)
+
+    ctx.reply(reply, { parse_mode: 'Markdown' })
+  } else {
+    ctx.reply(process.env.MESSAGE_UNKNOWN_USER as string)
+  }
+})
+
+bot.start()
